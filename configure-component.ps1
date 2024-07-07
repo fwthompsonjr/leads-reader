a14734c8319251ca8d2cb2c4cd509e06c6116fe7
Frank Thompson <Frank.Thompson@wolterskluwer.com>
2024-07-07 14:12:53 -0500

    edit release workflow to publish server component

---
+++ configure-component.ps1
@@ -0,0 +1,31 @@
+﻿<#
+    configure component
+#>
+param(
+    [bool]$isServer = $false,
+    [bool]$isHeadless = $true,
+    [bool]$applyUpdate = $false
+)
+$workfolder = [System.IO.Path]::GetDirectoryName( $MyInvocation.MyCommand.Path );
+$operationsFile = [System.IO.Path]::Combine( $workfolder, "src/worker/_configuration/operation-mode.txt" );
+$appSettingsFile = [System.IO.Path]::Combine( $workfolder, "src\worker\appsettings.json" );
+if ( [System.IO.File]::Exists( $operationsFile ) -eq $false ) { return; }
+if ( [System.IO.File]::Exists( $appSettingsFile ) -eq $false ) { return; }
+
+Write-Host "Configuration is found"
+$modeName = "single";
+if ($isServer -eq $true) { $modeName = "server"; }
+$ops = @{ 
+    "mode" = $modeName 
+    "headless" = $isHeadless
+    } | ConvertTo-Json;
+
+$config = [System.IO.File]::ReadAllText( $appSettingsFile ) | ConvertFrom-Json
+if ( $null -eq $config.OperationMode ) { return; }
+
+$config.OperationMode = $modeName;
+$settings = ($config | ConvertTo-Json);
+if ($applyUpdate -ne $true ) { return; }
+
+[System.IO.File]::WriteAllText( $operationsFile, $ops );
+[System.IO.File]::WriteAllText( $appSettingsFile, $settings );
