e12c60a0940a5f980d0420e6e6660b634adb4324
Frank Thompson <frank.thompson.jr@gmail.com>
2024-07-20 10:47:27 -0500

    feat: creat script to persist local user settings

---
+++ configure-git-user-settings.ps1
@@ -0,0 +1,32 @@
+﻿$setting = @{
+    "email" = "frank.thompson.jr@gmail.com"
+    "name" = "Frank Thompson"
+}
+$locations = @(
+    "C:\_g\leads-reader",
+    "C:\_g\lui\fwthompsonjr\leads-ui",
+    "C:\_d\lead-old"
+);
+$readOnly = $true
+$currentLc = Get-Location
+try
+{
+    $uemail = $setting.email;
+    $uname = $setting.name;
+
+    foreach( $loc in $locations )
+    {
+        Write-Output "User settings for folder $loc";
+        Set-Location $loc
+        if ($readOnly -eq $false )
+        {
+            git config user.email $uemail
+            git config user.name $uname
+        }
+        git config --get user.email 
+        git config --get user.name
+    }
+}
+finally {
+    Set-Location $currentLc
+}
